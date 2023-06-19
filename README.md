226fe487fa33ba482506fd803bebb3ad15aa358c
guscarrillo <115580058+guscarrillo@users.noreply.github.com>
2023-06-18 18:29:15 -0600

    added basic redux file structure

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+This project is a tutorial for basic redux by [Coding Addict](https://www.youtube.com/@CodingAddict).
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -4,14 +4,38 @@ import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 // items
 import cartItems from "./cart-items";
+import { createStore } from "redux";
+import { DECREASE, INCREASE } from "./actions";
+import reducer from "./reducers";
 // redux stuff
+
+//initial store
+const initialStore = {
+  count: 0,
+  name: 'john'
+}
+
+//store setup
+const store = createStore(reducer,initialStore);
+
+// reducer(10,'DECREASE');
+
+//using dispatch
+store.dispatch({type: DECREASE});
+// store.dispatch({type: 'DECREASE'});
+
+store.dispatch({type: INCREASE});
+store.dispatch({type: INCREASE});
+
+console.log(store.getState());
+
 
 function App() {
   // cart setup
 
   return (
     <main>
-      <Navbar />
+      <Navbar cart={store.getState()} />
       <CartContainer cart={cartItems} />
     </main>
   );
